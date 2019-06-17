3b4c89a313734b678f26b1e003e8062cf629da52
tumao2 <tuminglong@126.com>
2019-06-17 15:19:52 +0800

    优化MQ配置

--- hdw-upms-web/src/main/java/com/hdw/mq/config/ActiveMQReceiveMsgService.java
+++ hdw-upms-web/src/main/java/com/hdw/mq/config/ActiveMQReceiveMsgService.java
@@ -3,16 +3,14 @@ package com.hdw.mq.config;
 import com.hdw.common.utils.JacksonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.advisory.AdvisorySupport;
-import org.apache.activemq.command.ActiveMQMessage;
-import org.apache.activemq.command.ConnectionInfo;
-import org.apache.activemq.command.ConsumerInfo;
-import org.apache.activemq.command.ProducerInfo;
+import org.apache.activemq.command.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.stereotype.Component;
 
 import javax.jms.*;
+import javax.jms.Message;
 
 /**
  * @Description ActiveMQ 生产者/消费者模式 消费者服务类 发布/订阅模式  订阅者模式
@@ -33,7 +31,7 @@ public class ActiveMQReceiveMsgService {
      */
     @JmsListener(destination = "test", containerFactory = "queueJmsListenerContainerFactory", concurrency = "5-10")
     public void receiveMsg(String text) {
-        System.out.println("==== 生产者/消费者模式 收到的消息：" + text + " ====");
+        log.info("==== 生产者/消费者模式 收到的消息：" + text + " ====");
     }
 
 
@@ -44,17 +42,63 @@ public class ActiveMQReceiveMsgService {
      */
     @JmsListener(destination = "testTopic", containerFactory = "topicJmsListenerContainerFactory", concurrency = "5-10")
     public void receiveTopicMsg(String text) {
-        System.out.println("==== 发布/订阅模式 收到的消息：" + text + " ====");
+        log.info("==== 发布/订阅模式 收到的消息：" + text + " ====");
     }
 
 
     @JmsListener(destination = "ActiveMQ.Advisory.Connection", containerFactory = "topicJmsListenerContainerFactory",concurrency = "5-10")
-    public void onMessage(Message msg) {
+    public void advisoryConnection(Message msg) {
         if (msg instanceof ActiveMQMessage) {
             try {
                 ActiveMQMessage aMsg = (ActiveMQMessage) msg;
-                ConnectionInfo consumerInfo = (ConnectionInfo) aMsg.getDataStructure();
-                System.out.println("连接信息："+JacksonUtils.toJson(consumerInfo));
+                ConnectionInfo connectionInfo = (ConnectionInfo) aMsg.getDataStructure();
+                log.info("连接信息：" + JacksonUtils.toJson(connectionInfo));
+                // 告知activemq应用已收到消息
+                msg.acknowledge();
+            } catch (JMSException e) {
+                log.error("Failed to process message: " + msg);
+            }
+        }
+    }
+
+
+    @JmsListener(destination = "ActiveMQ.Advisory.Queue", containerFactory = "topicJmsListenerContainerFactory", concurrency = "5-10")
+    public void advisoryQueueMessage(Message msg) {
+        if (msg instanceof ActiveMQMessage) {
+            try {
+                ActiveMQMessage aMsg = (ActiveMQMessage) msg;
+                DestinationInfo destinationInfo = (DestinationInfo) aMsg.getDataStructure();
+                log.info("队列信息：" + JacksonUtils.toJson(destinationInfo));
+                // 告知activemq应用已收到消息
+                msg.acknowledge();
+            } catch (JMSException e) {
+                log.error("Failed to process message: " + msg);
+            }
+        }
+    }
+
+    @JmsListener(destination = "ActiveMQ.Advisory.Consumer.Queue.test", containerFactory = "topicJmsListenerContainerFactory", concurrency = "5-10")
+    public void advisoryConsumerQueueTest(Message msg) {
+        if (msg instanceof ActiveMQMessage) {
+            try {
+                ActiveMQMessage aMsg = (ActiveMQMessage) msg;
+                ConsumerInfo consumerInfo = (ConsumerInfo) aMsg.getDataStructure();
+                log.info("消费者信息：" + JacksonUtils.toJson(consumerInfo));
+                // 告知activemq应用已收到消息
+                msg.acknowledge();
+            } catch (JMSException e) {
+                log.error("Failed to process message: " + msg);
+            }
+        }
+    }
+
+    @JmsListener(destination = "ActiveMQ.Advisory.Producer.Queue.test", containerFactory = "topicJmsListenerContainerFactory", concurrency = "5-10")
+    public void advisoryProducerQueueTest(Message msg) {
+        if (msg instanceof ActiveMQMessage) {
+            try {
+                ActiveMQMessage aMsg = (ActiveMQMessage) msg;
+                ProducerInfo producerInfo = (ProducerInfo) aMsg.getDataStructure();
+                log.info("生产者信息：" + JacksonUtils.toJson(producerInfo));
                 // 告知activemq应用已收到消息
                 msg.acknowledge();
             } catch (JMSException e) {
