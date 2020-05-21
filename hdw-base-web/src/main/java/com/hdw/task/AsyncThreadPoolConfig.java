34b46e672e14593586995fb069d27ba4210ac1b5
毛毛不好学 <tuminglong@126.com>
2020-05-21 20:12:30 +0800

    优化

--- hdw-base-web/src/main/java/com/hdw/task/AsyncThreadPoolConfig.java
+++ hdw-base-web/src/main/java/com/hdw/task/AsyncThreadPoolConfig.java
@@ -1,6 +1,5 @@
 package com.hdw.task;
 
-import org.apache.tomcat.util.threads.ThreadPoolExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -8,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @Description Spring 异步线程池配置
@@ -39,7 +39,7 @@ public class AsyncThreadPoolConfig {
         // 设置默认线程名称
         executor.setThreadNamePrefix(env.getProperty("spring.application.name") + "-");
         // 设置拒绝策略
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         // 等待所有任务结束后再关闭线程池
         executor.setWaitForTasksToCompleteOnShutdown(true);
         return executor;
